ce2947750777c20c543f18ae5582ed34913f050f
phuocloi0502 <birdverybig103@gmail.com>
2020-05-14 10:56:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bai5/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bai5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai8_3);
+//        Button bt=findViewById(R.id.btn);
+//        final EditText editText=findViewById(R.id.ed);
+//        bt.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,editText.getText(),Toast.LENGTH_LONG).show();
+//            }
+//        });
+    }
+}
